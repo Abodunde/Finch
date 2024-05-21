f7b2cc014aa70b4e6549eaa629444c47e2fd95ba
Abodunde Ojo <72313649+Abodunde@users.noreply.github.com>
2024-05-21 16:05:24 +0100

    Update text_classification_app.py

--- text_classification_app.py
+++ text_classification_app.py
@@ -17,7 +17,7 @@ import re
 from nltk.corpus import wordnet
 from sklearn.base import BaseEstimator, TransformerMixin
 nltk_resources = ["punkt", "wordnet", "averaged_perceptron_tagger"]
-imprt nltk
+import nltk
 for resource in nltk_resources:
     try:
         nltk.data.find(resource)
